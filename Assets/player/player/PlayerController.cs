b8a453cb690b842a7848073645a703bcedbc5b05
ddogg231 <116034321+ddogg231@users.noreply.github.com>
2023-02-03 23:47:40 -0500

    Initial commit

---
+++ Assets/player/player/PlayerController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
+public class PlayerController : MonoBehaviour
+{
+    //Components
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer sr;
+
+    //movement var
+    public float speed;
+    public float jumpForce;
+
+    //groundcheck stuff
+    public bool isGrounded;
+    public Transform groundCheck;
+    public LayerMask isGroundLayer;
+    public float groundCheckRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
+        if (speed <= 0)
+        {
+            speed = 6.0f;
+            Debug.Log("Speed was set incorrect, defaulting to " + speed.ToString());
+        }
+
+        if (jumpForce <= 0)
+        {
+            jumpForce = 300;
+            Debug.Log("Jump force was set incorrect, defaulting to " + jumpForce.ToString());
+        }
+
+        if (groundCheckRadius <= 0)
+        {
+            groundCheckRadius = 0.2f;
+            Debug.Log("Ground Check Radius was set incorrect, defaulting to " + groundCheckRadius.ToString());
+        }
+
+        if (!groundCheck)
+        {
+            groundCheck = GameObject.FindGameObjectWithTag("GroundCheck").transform;
+            Debug.Log("Ground Check not set, finding it manually!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimatorClipInfo[] curPlayingClip = anim.GetCurrentAnimatorClipInfo(0);
+        float hInput = Input.GetAxisRaw("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+
+        if (curPlayingClip.Length > 0)
+        {
+            if (Input.GetButtonDown("Fire1") && curPlayingClip[0].clip.name != "Fire")
+            {
+                anim.SetTrigger("fire");
+            }
+            else if (curPlayingClip[0].clip.name == "Fire")
+            {
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
+                rb.velocity = moveDirection;
+            }
+        }
+        
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        if (!isGrounded && Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("JumpAttack");
+        }
+
+        
+        anim.SetFloat("hInput", Mathf.Abs(hInput));
+        anim.SetBool("isGrounded", isGrounded);
+
+        //check for flipped and create an algorithm to flip your character
+        if (hInput != 0)
+            sr.flipX = (hInput < 0);
+    }
+}
